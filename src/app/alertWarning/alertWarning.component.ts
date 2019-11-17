15f82d63138059757ebcf6b7fe932246ce6cf93c
Chedro Cardenas <chegiorca@hotmail.com>
2019-11-16 17:45:37 -0700

    first assigemnet on Udemy - Angular

---
+++ src/app/alertWarning/alertWarning.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'alertWarning',
+    templateUrl:'./alertWarning.component.html',
+    styleUrls:['./alertWarning.component.css']
+})
+
+export class AlertWarning{
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+
+import { FormsModule } from "@angular/forms";
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { AlertWarning} from './alertWarning/alertWarning.component';
+import { AlertSuccessComponent } from './alert-success/alert-success.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    AlertWarning,
+    AlertSuccessComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
