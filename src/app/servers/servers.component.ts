10ed439e3f5b65e2dd5b4e4822aa55b2a31379c3
Chedro Cardenas <chegiorca@hotmail.com>
2019-11-17 09:07:44 -0700

    assignment 2 Completed

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,21 +9,29 @@ export class ServersComponent implements OnInit {
 allowNewServer = false;
 serverCreation = 'No Server was created';
 serverName = '';
+username = '';
+port =''
   constructor() { 
     setTimeout(()=>{
       this.allowNewServer = true;
     },2000)
+    
   }
 
   ngOnInit() {
   }
 
   onCreateServer(){
-    this.serverCreation = " Server was created";
+    this.serverCreation = " Server was created " + this.serverName;
 
   }
-  onUpdateServerName(event: any){
+  onUpdateServerName(event: Event){
     console.log(event); //checking at the console the object
     this.serverName = (<HTMLInputElement>event.target).value; // getting into the event
+    //inform TS that the event is an HTML
+  }
+
+  onLogIn(){
+    this.username ='';  
   }
 }
